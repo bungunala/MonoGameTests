a11374b1bf03f4c84e6722a6e9db9c07b738d26d
bungunala <bungunala@hotmail.com>
2022-10-11 16:56:16 -0500

    Primeros pasos

--- Ponga/Game1.cs
+++ Ponga/Game1.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Ponga
 {
     public class Game1 : Game
     {
+        Texture2D ballTexture;
+        Vector2 ballPosition;
+        float ballSpeed;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -19,6 +24,8 @@ namespace Ponga
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            ballPosition = new Vector2(_graphics.PreferredBackBufferWidth/2, _graphics.PreferredBackBufferHeight/2);
+            ballSpeed = 100f;
 
             base.Initialize();
         }
@@ -26,16 +33,15 @@ namespace Ponga
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            ballTexture = Content.Load <Texture2D> (@"gfx/ball");            
 
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
             // TODO: Add your update logic here
+            updateInput(gameTime);
 
             base.Update(gameTime);
         }
@@ -45,8 +51,28 @@ namespace Ponga
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(ballTexture, ballPosition,null, Color.White,0f,new Vector2(ballTexture.Width/2,ballTexture.Height/2),Vector2.One,SpriteEffects.None,0f);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        protected void updateInput(GameTime gameTime)
+        {
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
         }
     }
 }
